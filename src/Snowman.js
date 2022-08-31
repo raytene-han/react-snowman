b86ce2b6465b64c4c98d45d61d33d5fae3b75527
Marshall Buck <dev@marshallbuck.com>
2022-08-31 14:11:57 -0500

    done

--- src/Snowman.js
+++ src/Snowman.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import { randomWord, ENGLISH_WORDS } from "./words";
+
 import "./Snowman.css";
 import img0 from "./0.png";
 import img1 from "./1.png";
@@ -24,23 +26,23 @@ import img6 from "./6.png";
  */
 
 function Snowman({
-      images=[img0, img1, img2, img3, img4, img5, img6],
-      words=["apple"],
-      maxWrong=6,
-    }) {
+  images = [img0, img1, img2, img3, img4, img5, img6],
+  words = ENGLISH_WORDS,
+  maxWrong = 6,
+}) {
   /** by default, allow 6 guesses and use provided gallows images. */
 
   const [nWrong, setNWrong] = useState(0);
   const [guessedLetters, setGuessedLetters] = useState(() => new Set());
-  const [answer, setAnswer] = useState((words)[0]);
+  const [answer, setAnswer] = useState(randomWord(words));
 
   /** guessedWord: show current-state of word:
    if guessed letters are {a,p,e}, show "app_e" for "apple"
    */
   function guessedWord() {
     return answer
-        .split("")
-        .map(ltr => (guessedLetters.has(ltr) ? ltr : "_"));
+      .split("")
+      .map(ltr => (guessedLetters.has(ltr) ? ltr : "_"));
   }
 
   /** handleGuess: handle a guessed letter:
@@ -62,25 +64,33 @@ function Snowman({
   /** generateButtons: return array of letter buttons to render */
   function generateButtons() {
     return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
-        <button
-            key={ltr}
-            value={ltr}
-            onClick={handleGuess}
-            disabled={guessedLetters.has(ltr)}
-        >
-          {ltr}
-        </button>
+      <button
+        key={ltr}
+        value={ltr}
+        onClick={handleGuess}
+        disabled={guessedLetters.has(ltr)}
+      >
+        {ltr}
+      </button>
     ));
   }
 
+  /** handles restart game click */
+  function resetGame() {
+    setNWrong(0);
+    setGuessedLetters(new Set());
+    setAnswer(randomWord(words));
+  }
+
   return (
-      <div className="Snowman">
-        <img src={(images)[nWrong]} alt={nWrong} />
-        <p>Number of wrong guesses: {nWrong}</p>
-        <p className="Snowman-word">{guessedWord()}</p>
-        {nWrong !== maxWrong && <p>{generateButtons()}</p>}
-        {nWrong === maxWrong && <p id="lose">You lose: {answer}</p>}
-      </div>
+    <div className="Snowman">
+      <img src={(images)[nWrong]} alt={nWrong} />
+      <p>Number of wrong guesses: {nWrong}</p>
+      <p className="Snowman-word">{guessedWord()}</p>
+      {nWrong !== maxWrong && <p>{generateButtons()}</p>}
+      {nWrong === maxWrong && <p id="lose">You lose: {answer}</p>}
+      <button onClick={resetGame}>Reset Game</button>
+    </div>
   );
 }
 
--- src/Snowman.test.js
+++ src/Snowman.test.js
@@ -8,7 +8,7 @@ import img4 from "./4.png";
 import img5 from "./5.png";
 import img6 from "./6.png";
 
-it("disallows guessing after max guesses reached", function() {
+it("disallows guessing after max guesses reached", function () {
   const { container } = render(
     <Snowman
       images={[img0, img6]}
@@ -20,9 +20,65 @@ it("disallows guessing after max guesses reached", function() {
   const button = container.querySelector('button[value="a"]');
   fireEvent.click(button);
   // expect the length of button elements to be 0
-  expect(container.querySelectorAll('button').length).toEqual(0)
+  expect(container.querySelectorAll('button').length).toEqual(1);
   expect(container.querySelector('#lose')).toBeInTheDocument();
 
+});
+
+
+
+it("changes to correct image after incorrect guess", function () {
+  const { container } = render(
+    <Snowman
+      images={[img0, img2]}
+      words={["bet"]}
+      maxWrong={2}
+    />
+  );
+  const button = container.querySelector('button[value="a"]');
+  fireEvent.click(button);
+  const img = container.querySelector('img').getAttribute('src');
+  expect(img).toContain("2.png");
+  expect(img).not.toContain("0.png");
+
+});
+
+
+it("changes to correct image after correct guess", function () {
+  const { container } = render(
+    <Snowman
+      images={[img0, img2]}
+      words={["bet"]}
+      maxWrong={2}
+    />
+  );
+  const button = container.querySelector('button[value="b"]');
+  fireEvent.click(button);
+  const img = container.querySelector('img').getAttribute('src');
+  expect(img).toContain("0.png");
+  expect(img).not.toContain("2.png");
+
+});
+
+
+it("shows correct word and underscores", function () {
+  const { container } = render(
+    <Snowman
+      images={[img0, img2]}
+      words={["bet"]}
+      maxWrong={2}
+    />
+  );
+  const firstClick = container.querySelector('button[value="b"]');
+  fireEvent.click(firstClick);
+  const word = container.querySelector('.Snowman-word').innerHTML;
+  expect(word).toEqual("b__");
+
+  const secondClick = container.querySelector('button[value="t"]');
+
+  fireEvent.click(secondClick);
+  const secondGuess = container.querySelector('.Snowman-word').innerHTML;
+  expect(secondGuess).toEqual("b_t");
 });
 
 
@@ -35,32 +91,32 @@ it("renders without crashing", function () {
       words={["test"]}
       maxWrong={6}
     />
-  )
+  );
 });
 
 
 it("matches snapshot", function () {
   const { container } =
-  render(
-    <Snowman
-      images={[img0, img1, img2, img3, img4, img5, img6]}
-      words={["test"]}
-      maxWrong={6}
-    />
-  )
+    render(
+      <Snowman
+        images={[img0, img1, img2, img3, img4, img5, img6]}
+        words={["test"]}
+        maxWrong={6}
+      />
+    );
   expect(container).toMatchSnapshot();
 
 });
 
 it("matches snapshot for losing", function () {
   const { container } =
-  render(
-    <Snowman
-      images={[img0, img6]}
-      words={["x"]}
-      maxWrong={1}
-    />
-  )
+    render(
+      <Snowman
+        images={[img0, img6]}
+        words={["x"]}
+        maxWrong={1}
+      />
+    );
 
   const button = container.querySelector('button[value="a"]');
   fireEvent.click(button);
